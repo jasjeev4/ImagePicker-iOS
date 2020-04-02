68893853bca4ae88776d8e2ec3c47d50eb03f56a
Jasjeev <jasjeev4@gmail.com>
2020-04-02 20:32:49 +0530

    Added support to move content on keyboard on screen.

--- ImagePicker/ViewController.swift
+++ ImagePicker/ViewController.swift
@@ -33,6 +33,18 @@ UINavigationControllerDelegate{
         
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        //Subscribe to Keyboard notifications
+        
+        subscripbeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
+    }
 
 //    @IBAction func pickAnImage(_ sender: Any) {
 //        let imagePicker = UIImagePickerController()
@@ -76,6 +88,28 @@ UINavigationControllerDelegate{
         bottomText.text = ""
     }
     
+    @objc func keyboardWillShow(_ notification: Notification) {
+        view.frame.origin.y = -getKeyboardHeight(notification)
+    }
     
+    func getKeyboardHeight(_ notification: Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.cgRectValue.height
+    }
+    
+    func subscripbeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        view.frame.origin.y = 0
+    }
 }
 
