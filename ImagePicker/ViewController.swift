86929efbb3f3b5807226dfd33707997e5b820307
Jasjeev <jasjeev4@gmail.com>
2020-04-02 17:29:02 +0530

    Added permissions

--- ImagePicker/ViewController.swift
+++ ImagePicker/ViewController.swift
@@ -10,21 +10,42 @@ import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate,
 UINavigationControllerDelegate{
-    let imagePickerDelegate = ImagePickerDelegate()
     
-    @IBOutlet weak var imageView: UIImagePickerController!
+    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
         // Do any additional setup after loading the view.
     }
 
     @IBAction func pickAnImage(_ sender: Any) {
         let imagePicker = UIImagePickerController()
-        
-        imagePicker.delegate = self.imagePickerDelegate
-        
+                
         // imagePicker.delegate = imageViewDelegate
+        present(imagePicker, animated: true, completion: nil)
+        
+        imagePicker.delegate =  self
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                                        didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        print("Succesfully picked image")
+        
+    }
+    
+    @IBAction func pickAnImageFromCamera(_ sender: Any) {
+
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func pickAnImageFromAlbum(_ sender: Any) {
+
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
         present(imagePicker, animated: true, completion: nil)
     }
     
